ea8d624424e216976f5f6080543d60991312e0f3
Daniel Antonio <dernlsenpai@gmail.com>
2020-10-14 20:44:43 -0700

    Added item functionality, functionality testing, and finished stretch

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,38 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  if(item.enhancements !== 20) {
+    return { ...item, enhancements: item.enhancements + 1 };
+  } else {
+    return { ...item }
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  let updatedItem;
+  if(item.enhancements < 15) {
+    updatedItem = { ...item, enhancements: item.enhancements - 5 }
+  } else if (item.enhancements > 16) {
+    updatedItem = { ...item, enhancements: item.enhancements - 1 }
+  } else if (item.enhancements > 15) {
+    updatedItem = { ...item, enhancements: item.enhancements - 10 }
+  }
+
+  if(updatedItem.enhancements < 0) {
+    return { ...updatedItem, enhancements: 0 }
+  } else {
+    return updatedItem;
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
-  return { ...item };
-}
+  if(item.enhancements > 0) {
+    return { ...item, name: `[+${item.enhancements}]${item.name}`};
+  } else {
+    return { ...item }
+  }
+};
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,72 @@
+const { TestScheduler } = require('jest');
 const enhancer = require('./enhancer.js');
 // test away!
+
+test('repair item', () => {
+    const item = {
+        name: 'Great Scythe',
+        durability: 45,
+        enhancements: 0
+    }
+
+    const repairItem = enhancer.repair(item);
+
+    expect(repairItem.durability).toBe(100)
+});
+
+test('successfully enhance weapon.', () => {
+    const item = {
+        name: 'Great Scythe',
+        durability: 45,
+        enhancements: 0
+    }
+
+    const enhanceItem = enhancer.success(item);
+
+    expect(enhanceItem.enhancements).toBe(item.enhancements + 1);
+});
+
+test('return same value at success max enhancement', () => {
+    const item = {
+        name: 'Great Scythe',
+        durability: 45,
+        enhancements: 20
+    }
+
+    const enhanceItem = enhancer.success(item);
+
+    expect(enhanceItem.enhancements).toBe(20);
+});
+
+test('fail enhance weapon at 0', () => {
+    const item = {
+        name: 'Great Scythe',
+        durability: 45,
+        enhancements: 0
+    }
+
+    const unhanceItem = enhancer.fail(item);
+    
+    expect(unhanceItem.enhancements).toBe(0);
+});
+
+test('change name if item is enhanced', () => {
+    const items = [
+        {
+            name: 'Zweihander',
+            durability: 100,
+            enhancements: 20
+        },
+        {
+            name: 'Great Scythe',
+            durability: 45,
+            enhancements: 0
+        }
+    ];
+
+    const plusWeapon = enhancer.get(items[0]);
+    const normalWeapon = enhancer.get(items[1]);
+
+    expect(plusWeapon.name).toMatch(/\d+/);
+    expect(normalWeapon.name).not.toMatch(/\d+/);
+});
